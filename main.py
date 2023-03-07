ecd1134901cf472adc0f961a23042f6f4209df35
Mark Macaraeg <markmcrg9@gmail.com>
2023-03-07 16:46:35 +0800

    Added undetected chromedriver to bypass cloudflare

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager 
-from selenium.webdriver.chrome.service import Service as ChromeService 
+import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+options = uc.ChromeOptions() 
 
-options = Options()
+# options.headless = True 
 
 # Change to your own path to the chrome profile
 options.add_argument("user-data-dir=C:\\Users\\Mark\\AppData\\Local\\Google\\Chrome\\User Data") 
@@ -13,10 +13,11 @@ options.add_argument("user-data-dir=C:\\Users\\Mark\\AppData\\Local\\Google\\Chr
 # Change to your own profile name
 options.add_argument('--profile-directory=Profile 1')
 
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install())) 
+driver = uc.Chrome(use_subprocess=True, options=options) 
+driver.maximize_window() 
 driver.get('https://app.datacamp.com/groups/gdsc-pup-main/leaderboard')
 
-driver.find_element(By.XPATH, '//*[@id="accountModal"]/div/div/div[3]/div/div/a[3]').click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='accountModal']/div/div/div[3]/div/div/a[3]"))).click()
 
 time.sleep(10)
 driver.quit()
